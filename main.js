311cbee30521bffbfb7c44d48d924d6e7348bada
elamanz <z.elaman699@gmail.com>
2023-11-09 13:17:47 +0600

    test

--- main.js
+++ main.js
@@ -9,3 +9,6 @@ favoriteBtn.addEventListener('click', () => {
     }
     console.log('click')
 })
+
+const API_KEY = 'f8b7220a-ec1b-477a-a473-19c5fd9c1329'
+const API_URL_PREMIERES = "https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=2023&month=NOVEMBER"
