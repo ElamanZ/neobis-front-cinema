4db58e37132ce3b099044a74ec49e51acb328ccb
ElamanZ <z.elaman699@gmail.com>
2023-11-13 03:21:53 +0600

    done favorites

--- main.js
+++ main.js
@@ -1,27 +1,26 @@
 const moviesContent = document.getElementById('movies-content');
 const form = document.querySelector("form");
 const searchInput = document.querySelector(".header__input");
-const premiereMoviesBtn = document.getElementById('premiereMovies')
-const anticipatedMoviesBtn = document.getElementById('anticipatedMovies')
-const popularMoviesBtn = document.getElementById('popularMovies')
-const realesesMonthMoviesBtn = document.getElementById('realesesMonthMovies')
-const favoriteMoviesBtn = document.getElementById('favoriteMovies')
+const premiereMoviesBtn = document.getElementById('premiereMovies');
+const anticipatedMoviesBtn = document.getElementById('anticipatedMovies');
+const popularMoviesBtn = document.getElementById('popularMovies');
+const realesesMonthMoviesBtn = document.getElementById('realesesMonthMovies');
+const favoriteMoviesBtn = document.getElementById('favoriteMovies');
 
 const infoDate = new Date();
 const monthOfYear = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"];
-const month = monthOfYear[infoDate.getMonth()]
-const year = infoDate.getFullYear()
-console.log(month, year)
+const month = monthOfYear[infoDate.getMonth()];
+const year = infoDate.getFullYear();
 
 const API_KEY = 'f8b7220a-ec1b-477a-a473-19c5fd9c1329';
 const API_URL_PREMIERES = `https://kinopoiskapiunofficial.tech/api/v2.2/films/premieres?year=${year}&month=${month}`;
-const API_URL_ANTICIPADET = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=CLOSES_RELEASES&page=1';
+const API_URL_ANTICIPATED = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=CLOSES_RELEASES&page=1';
 const API_URL_SEARCH = "https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=";
 const API_URL_POPULAR = "https://kinopoiskapiunofficial.tech/api/v2.2/films/collections?type=TOP_POPULAR_ALL&page=1";
-const API_URL_REALASES = `https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=${year}&month=${month}`
-
+const API_URL_RELEASES = `https://kinopoiskapiunofficial.tech/api/v2.1/films/releases?year=${year}&month=${month}`;
 
 getMovies(API_URL_PREMIERES);
+
 async function getMovies(url) {
     try {
         const resp = await fetch(url, {
@@ -32,46 +31,33 @@ async function getMovies(url) {
         });
         const respData = await resp.json();
         showMovies(respData);
-        console.log(respData)
     } catch (error) {
         console.error(error);
     }
-
 }
-
-
 
 function getClassByRate(rating) {
     if (rating >= 7) {
-        return "green"
+        return "green";
     } else if (rating > 5) {
-        return "orange"
+        return "orange";
     } else if (rating < 5) {
-        return "red"
+        return "red";
     }
 }
 
-function toggleFavorite(index) {
-    const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
-    const movieIndex = favoriteMovies.indexOf(index);
 
-    if (movieIndex === -1) {
-        favoriteMovies.push(index);
-    } else {
-        favoriteMovies.splice(movieIndex, 1);
-    }
 
-    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
-}
 
 function showMovies(data) {
     moviesContent.innerHTML = "";
     const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
 
     const items = data.releases || data.films || data.items;
-    items.slice(0, 30).forEach((movie, index) => {
-        const isFavorite = favoriteMovies.includes(index);
+    items.slice(0, 30).forEach((movie) => {
+        const isFavorite = favoriteMovies.some(favMovie => favMovie.kinopoiskId === movie.kinopoiskId);
         const movieElement = document.createElement("div");
+        movieElement.dataset.kinopoiskId = movie.kinopoiskId; // Add the data-kinopoisk-id attribute
         movieElement.innerHTML = `
             <div class="movie">
                 <div class="movie__cover-inner">
@@ -93,7 +79,8 @@ function showMovies(data) {
                     </div>
                     <div class="movie_content-favorite">
                         <img 
-                            id="favorite-btn"
+                            class="favorite-btn"
+                            data-kinopoisk-id="${movie.kinopoiskId}"
                             src="${isFavorite ? 'image/heart__red.png' : 'image/heart__white.png'}"
                             alt="logo_favorite"
                        
@@ -103,9 +90,15 @@ function showMovies(data) {
             </div>
         `;
 
-        movieElement.querySelector("#favorite-btn").addEventListener("click", (e) => {
+        const favoriteBtn = movieElement.querySelector(".favorite-btn");
+
+        favoriteBtn.addEventListener("click", (e) => {
             e.preventDefault();
-            toggleFavorite(index);
+            if (isFavorite) {
+                removeFavorite(movie.kinopoiskId);
+            } else {
+                toggleFavorite(movie);
+            }
             showMovies(data);
         });
 
@@ -113,70 +106,71 @@ function showMovies(data) {
     });
 }
 
+function toggleFavorite(movie) {
+    const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+    const movieIndex = favoriteMovies.findIndex(favMovie => favMovie.kinopoiskId === movie.kinopoiskId);
+
+    if (movieIndex === -1) {
+        favoriteMovies.push(movie);
+    } else {
+        favoriteMovies.splice(movieIndex, 1);
+    }
+
+    localStorage.setItem('favoriteMovies', JSON.stringify(favoriteMovies));
+
+    showMovies({ films: favoriteMovies });
+
+    return favoriteMovies;
+}
+
+function removeFavorite(kinopoiskId) {
+    const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
+    const updatedFavorites = favoriteMovies.filter(movie => movie.kinopoiskId !== kinopoiskId);
+    localStorage.setItem('favoriteMovies', JSON.stringify(updatedFavorites));
+
+    showMovies({ films: updatedFavorites });
+
+    return updatedFavorites;
+}
+
+
 
 favoriteMoviesBtn.addEventListener('click', (e) => {
     e.preventDefault();
     const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
-    const favoriteMoviesData = { films: favoriteMovies.map(index => data.releases[index]) };
-    showMovies(favoriteMoviesData);
+
+    if (favoriteMovies.length > 0) {
+        showMovies({ films: favoriteMovies });
+    } else {
+        console.log('No favorite movies.');
+    }
 });
 
-
-
-
-
-
-
-
-
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const apiSearchUrl = `${API_URL_SEARCH}${searchInput.value}`
+    e.preventDefault();
+    const apiSearchUrl = `${API_URL_SEARCH}${searchInput.value}`;
     if (searchInput.value) {
-        getMovies(apiSearchUrl)
-        searchInput.value = ""
+        getMovies(apiSearchUrl);
+        searchInput.value = "";
     }
-    return getMovies(apiSearchUrl)
-})
-
-favoriteMoviesBtn.addEventListener('click', (e) => {
-     e.preventDefault();
-     const favoriteMovies = JSON.parse(localStorage.getItem('favoriteMovies')) || [];
-     const favoriteMoviesData = { releases: favoriteMovies };
-
-     if (favoriteMoviesData.releases) {
-            showMovies(favoriteMoviesData);
-     } else {
-         console.error('No favorite movies found.');
-     }
-    });
-
+});
 
 premiereMoviesBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const premiereMovies = `${API_URL_PREMIERES}`
-    return getMovies(premiereMovies)
+    e.preventDefault();
+    getMovies(API_URL_PREMIERES);
+});
 
-})
 anticipatedMoviesBtn.addEventListener("click", (e) => {
-    e.preventDefault()
-    const anticipatedMovies = `${API_URL_ANTICIPADET}`
-    return getMovies(anticipatedMovies)
-})
+    e.preventDefault();
+    getMovies(API_URL_ANTICIPATED);
+});
 
 realesesMonthMoviesBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const realesesMonthMovies = `${API_URL_REALASES}`
-    return getMovies(realesesMonthMovies)
-})
+    e.preventDefault();
+    getMovies(API_URL_RELEASES);
+});
 
 popularMoviesBtn.addEventListener('click', (e) => {
-    e.preventDefault()
-    const popularMovies = `${API_URL_POPULAR}`
-    return getMovies(popularMovies)
-})
-
-
-
-
-
+    e.preventDefault();
+    getMovies(API_URL_POPULAR);
+});
